ca841b4c682c8057fc35f012dabbb34ad081999c
htuananh <anhnguyenhuutuan@vccorp.vn>
2022-08-24 12:12:38 +0700

    Thêm cơ chế tải file model và đọc biến từ sys argv

--- run.py
+++ run.py
@@ -1,9 +1,21 @@
 import os
+import requests
+import sys
 from datetime import datetime
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+URL_MODEL = 'https://huggingface.co/VietAI/gpt-neo-1.3B-vietnamese-news/blob/' \
+    'main/pytorch_model.bin'
 start_time = datetime.utcnow()
+trained_model_path = os.path.join(os.getcwd(), 'pytorch_model.bin')
+if not os.path.exists(trained_model_path):
+    with requests.get(URL_MODEL, stream=True) as trained_remote_file:
+        trained_remote_file.raise_for_status()
+        with open(trained_model_path, 'wb') as trained_local_file:
+            for chunk in trained_remote_file.iter_content(chunk_size=8192):
+                trained_local_file.write(chunk)
+
 tokenizer = AutoTokenizer.from_pretrained(os.getcwd())
 model = AutoModelForCausalLM.from_pretrained(
     os.getcwd(),
@@ -13,11 +25,12 @@ model = AutoModelForCausalLM.from_pretrained(
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-prompt = input("Nhập 1 đoạn nói về chủ đề bạn muốn tạo: ")
+max_length = sys.argv[1]
+prompt = sys.argv[2]
 while True:
-    max_length = input('Nhập số ký tự tối đa của đoạn văn được tạo: ')
     if not str(max_length).isdigit():
         print('Số ký tự phải là số nguyên (VD: 1000)')
+        max_length = input('Nhập số ký tự tối đa của đoạn văn được tạo: ')
         continue
     max_length = int(max_length)
     break
