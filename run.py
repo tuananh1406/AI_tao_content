d7f4a57b281ed2e187e92d75e6b103ccb2ac4114
htuananh <anhnguyenhuutuan@vccorp.vn>
2022-08-24 13:59:15 +0700

    Kiểm tra và tải model nếu chưa có

--- run.py
+++ run.py
@@ -1,20 +1,55 @@
 import os
 import requests
 import sys
+import progressbar
+
 from datetime import datetime
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-URL_MODEL = 'https://huggingface.co/VietAI/gpt-neo-1.3B-vietnamese-news/blob/' \
-    'main/pytorch_model.bin'
+URL_MODEL = 'https://huggingface.co/VietAI/gpt-neo-1.3B-vietnamese-news/' \
+    'resolve/main/pytorch_model.bin'
 start_time = datetime.utcnow()
 trained_model_path = os.path.join(os.getcwd(), 'pytorch_model.bin')
+
 if not os.path.exists(trained_model_path):
     with requests.get(URL_MODEL, stream=True) as trained_remote_file:
+        data_size = int(
+            trained_remote_file.headers.get('Content-Length').strip())
+        data_downloaded = 0
+
+        # Tạo progressbar
+        download_widget = [
+            'Đang tải model: ',
+            progressbar.Bar(
+                marker=progressbar.AnimatedMarker(
+                    markers='█',
+                    fill='█',
+                    fill_wrap='\x1b[32m{}\x1b[39m',
+                    marker_wrap='\x1b[32m{}\x1b[39m',
+                ),
+                left="[",
+                right=" ",
+            ),
+            progressbar.Percentage(),
+            " ",
+            progressbar.FileTransferSpeed(),
+            "] ",
+            " of {0}MB".format(int(round(data_size / 1024 / 1024, 2))),
+        ]
+        current_bar = progressbar.ProgressBar(
+            widgets=download_widget,
+            max_value=data_size,
+        ).start()
+
         trained_remote_file.raise_for_status()
         with open(trained_model_path, 'wb') as trained_local_file:
             for chunk in trained_remote_file.iter_content(chunk_size=8192):
+                data_downloaded += len(chunk)
+                current_bar.update(data_downloaded)
                 trained_local_file.write(chunk)
+
+        current_bar.finish(dirty=True)
 
 tokenizer = AutoTokenizer.from_pretrained(os.getcwd())
 model = AutoModelForCausalLM.from_pretrained(
@@ -34,6 +69,10 @@ while True:
         continue
     max_length = int(max_length)
     break
+
+print('Tạo đoạn văn cho:')
+print(f'"{prompt}"')
+
 input_ids = tokenizer(prompt, return_tensors="pt")['input_ids'].to(device)
 
 gen_tokens = model.generate(
